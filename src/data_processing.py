60ceafd9b561b0afa03f613b16325517908c5072
Jiao Zeng <zzejiao@gmail.com>
2025-06-04 00:28:23 -0700

    before merge boxes, update data_processing.py

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,5 +1,7 @@
 import re
 import json
+from bs4 import BeautifulSoup, Tag, NavigableString
+
 abbr_map = {
     "ACT":   "Acceptance and commitment therapy",
     "ADHD":  "Attention-deficit hyperactivity disorder",
@@ -53,227 +55,225 @@ abbr_map = {
     "USA":   "United States of America",
     "VNS":   "Vagus nerve stimulation",
     "WHO":   "World Health Organization",
-}
-
-# append definition to abbreviation
-def append_definition(match: re.Match) -> str:
-    abbr = match.group(1)
-    definition = abbr_map.get(abbr, "")
-    return f"{abbr} ({definition})"
+    }
 
 def find_graph_by_id(graphs, graph_id):
     """
     Find a graph by its ID in the graphs dictionary.
     """
-    graph_id = graph_id.lower().replace(".", " ").strip().replace(" ", "_")
-    print(f"Formatted graph ID: {graph_id}")
     for graph in graphs:
         if graph['metadata']['referee_id'] == graph_id:
             return graph['text']
         
     return None
-        
-with open("data/processed/graphs.json", "r", encoding="utf-8") as f:
-    graphs = json.load(f)
-    
-
-from bs4 import BeautifulSoup, Tag, NavigableString
-
-import sys
-import re
-import os
-import json
-
-chunk_id = 1
-
-filename = "data/raw/source.html"
-# if there's no input from command line, use the default filename
-# if len(sys.argv) > 1:
-    # filename = sys.argv[1]
-    
-with open(filename, "r", encoding="utf-8") as f:
-    html = f.read()
-
-
-# # I manually substitute the Level 1, Level 2, Level 3, Level 4 link with text like (Leve 1), (Level 2), (Level 3), (Level 4)
-level1 = 'https://cdn.ncbi.nlm.nih.gov/pmc/blobs/843a/11351064/62befe587468/10.1177_07067437241245384-img1.jpg'
-level2 = 'https://cdn.ncbi.nlm.nih.gov/pmc/blobs/843a/11351064/b9ea5ad77490/10.1177_07067437241245384-img2.jpg'
-level3 = 'https://cdn.ncbi.nlm.nih.gov/pmc/blobs/843a/11351064/5be38aafe33f/10.1177_07067437241245384-img3.jpg'
-level4 = 'https://cdn.ncbi.nlm.nih.gov/pmc/blobs/843a/11351064/68e56cd87632/10.1177_07067437241245384-img4.jpg'
-
-
-soup = BeautifulSoup(html, "html.parser")
-output = []
-
-
-# parse the h1 title
-title = soup.find("h1")
-if title:
-    title = title.decode_contents().replace('\n', '')
-    
-output.append({
-    "text": title,
-    "metadata":{
-    "section": "title",
-    "type": "title",
-    "chunk_index": chunk_id,
-    "headings": "Title",
-    "referenced_tables": [],
-    }
-})
-
-chunk_id += 1
-
-countlevel1 = 0
-countlevel2 = 0
-countlevel3 = 0
-countlevel4 = 0
-# parse the main body
-for p in soup.find_all("p"):
-    referenced_tables = set()
-    
-    #-----------------------replace the <img> tags---------------------
-    # we also manually delete the duplication in first occurrence mentioning Levels
-    for img in p.find_all('img'):
-        src = img.get('src')
-        if src == level1:
-            replacement_text = "(Level 1)"
-            countlevel1 += 1
-        elif src == level2:
-            replacement_text = "(Level 2)"
-            countlevel2 += 1
-        elif src == level3:
-            replacement_text = "(Level 3)"
-            countlevel3 += 1
-        elif src == level4:
-            replacement_text = "(Level 4)"
-            countlevel4 += 1
-        else:
-            continue    
-            
-        text_node = NavigableString(replacement_text)
-        img.replace_with(text_node)
-        referenced_tables.add('Table A')
-
-
-        
-        
-    # ----------------------get section id----------------------------------
-    parent_sec = p.find_parent(["section",'figure'], id=True)
-    sec_id = parent_sec.get("id") if parent_sec else None
-    
-    #TODO: I'm currently ignoring the HTML tables, becuase AIKA's processing them.
     
     
-    #-----------------------get the headings---------------------
-    # special case: # manually finding the "No heading" in the html file to fix the No heading issue
-    # delete the <div><\div> outside this to get the correct heading: <p>Protracted Discontinuation Symptoms and Hyperbolic Tapering Schedules.</p>
-    
-    # get the closest heading
-    heading = p.find_previous_sibling(lambda tag: bool(re.match(r'^h[2-6]$', tag.name)))
-    headings = heading.get_text(strip=True) if heading else 'No heading' 
-    smallest_heading = headings
-    
-    if 'fig' in sec_id:
-        referenced_tables.add(headings)
-    #while parent still has parents
-    while parent_sec:
-        # print(f"parent_sec: {parent_sec.get('id')}")
-        heading = parent_sec.find_previous_sibling(lambda tag: bool(re.match(r'^h[2-6]$', tag.name)))
-        if heading:
-            headings = heading.get_text(strip=True) + ' > ' + headings
-        parent_sec = parent_sec.find_parent("section", id=True)
 
-    headings = headings.strip().replace('\n', ' ')
-    
-
-    #-----------------------get the text---------------------
-    text = p.get_text(separator=' ', strip=True) # get only text
-    # text = p.decode_contents().replace('\n', ' ') #get the text with <href> links and other tags
-    
-    
-    #----------------------- mark Type of the text ------------------------
-    type = 'paragraph'
-    
-    if 'table' in sec_id:  # either table image or table in HTML format
-        if p.get('class') and 'img-box' in p.get('class'):
-            type = 'table image '
-        else: 
-            continue
-        # img_link = p.find('img').get('src')
-        # print("img_link: ", img_link)
-        # text = str(img_link)
-        ##################################
-        try:
-            print(f"Finding graph by ID: {smallest_heading}")
-            text = find_graph_by_id(graphs, smallest_heading)
-        except Exception as e:
-            print(f"Error finding graph by ID: {e}")
-            continue
-        ##################################
-
-        # I only have 2 here with parent = <figure>, most of img's parent are <section id = 'table...'>
-    elif p.get('class') and 'img-box' in p.get('class'):
-        type = 'figure image'
-        ##################################
-        try:
-            print(f"Finding graph by ID: {smallest_heading}")
-            text = find_graph_by_id(graphs, smallest_heading)
-        except Exception as e:
-            print(f"Error finding graph by ID: {e}")
-            continue
-        ##################################
-        # img_link = p.find('img').get('src')
-        # print("img_link: ", img_link)
-        # text = str(img_link)
-        # sec_id = p.find_parent("figure", id=True).get("id")
-    elif 'box' in sec_id: 
-        type = 'box'
-    
-    
-    
-    #----------------------- get referenced tables ------------------------
-    all_links = p.find_all('a')
-    for link in all_links:
-        href = link.get('href')
-        if href.startswith('#'):
-            referenced_tables.add(link.get_text(strip=True))
-            
-    # ----------------------- replace abbreviation with definition ------------------------
-    # \b(ACT|ADHD|AI|…)\b
-    pattern = re.compile(
-        r'\b(' + '|'.join(re.escape(k) for k in abbr_map.keys()) + r')\b'
-    )
-    text = pattern.sub(append_definition, text)
-    
-            
-    #----------------------- formate the chunks ------------------------
+def parse_title(soup):
+    """
+    Parse the title 
+    """
+    title = soup.find("h1")
+    if title:
+        title = title.decode_contents().replace('\n', '')
     chunk = {
-        "text": "From section: "+ headings + " > paragraph id: " + str(chunk_id) + "\n"+ text,
-        "metadata": {
-        "section": "https://pmc.ncbi.nlm.nih.gov/articles/PMC11351064/#" + sec_id,
-        "type": type,
-        "chunk_index": chunk_id,
-        "headings": headings,
-        "referenced_tables": list(referenced_tables),
+    "text": title,
+    "metadata":
+        {
+        "section": "title",
+        "type": "title",
+        "headings": "Title of the guideline document",
+        "referenced_tables": [],
+        "referee_id": ""
         }
     }
+    return chunk
+
+# def merge_boxes(guideline):
     
-    output.append(chunk)
-    
-    chunk_id += 1
-    
-    
-# print("count of levels: ", countlevel1, countlevel2, countlevel3, countlevel4)
-    import json
+
+def prepend_headings_to_text(guideline):
+    """give chunk_id to each chunk in the guideline and prepend headings to text"""
+    for i in range(len(guideline)):
+        guideline[i]['metadata']['chunk_id'] = i
+            # "text": "From section: "+ headings + " > paragraph id: " + str(chunk_id) + "\n"+ text,
+        guideline[i]['text'] = "From section: " + guideline[i]['metadata']['headings'] + " > paragraph id: " + str(i) + "\n" + guideline[i]['text']
+        
+    # return guideline
+
+# append definition to abbreviation
+# def append_definition(match: re.Match) -> str:
+def append_definition(guideline):
+    for i in range(len(guideline)):
+        text = guideline[i]['text']
+        # find all abbreviations in the text
+        matches = re.findall(r'\b([A-Z]{2,})\b', text)
+        for abbr in matches:
+            definition = abbr_map.get(abbr, "")
+            if definition:
+                text = text.replace(abbr, f"{abbr} ({definition})")
+        guideline[i]['text'] = text
+    # return guideline
 
 
-with open('data/processed/tables.json', 'r', encoding='utf-8') as f2:
-    tables = json.load(f2)
+def parse_main_article(soup, graphs, output):
+    ## I manually substitute the Level 1, Level 2, Level 3, Level 4 link with text like (Leve 1), (Level 2), (Level 3), (Level 4)
+    level1 = 'https://cdn.ncbi.nlm.nih.gov/pmc/blobs/843a/11351064/62befe587468/10.1177_07067437241245384-img1.jpg'
+    level2 = 'https://cdn.ncbi.nlm.nih.gov/pmc/blobs/843a/11351064/b9ea5ad77490/10.1177_07067437241245384-img2.jpg'
+    level3 = 'https://cdn.ncbi.nlm.nih.gov/pmc/blobs/843a/11351064/5be38aafe33f/10.1177_07067437241245384-img3.jpg'
+    level4 = 'https://cdn.ncbi.nlm.nih.gov/pmc/blobs/843a/11351064/68e56cd87632/10.1177_07067437241245384-img4.jpg'
+    
+    # parse the main body
+    for p in soup.find_all("p"):
+        referenced_tables = set()
+        referee_id = ""
+        sec_id = ""
+        
+        #-----------------------replace the <img> tags---------------------
+        # we also manually delete the duplication in first occurrence mentioning Levels
+        for img in p.find_all('img'):
+            src = img.get('src')
+            if src == level1:
+                replacement_text = "(Level 1)"
+            elif src == level2:
+                replacement_text = "(Level 2)"
+            elif src == level3:
+                replacement_text = "(Level 3)"
+            elif src == level4:
+                replacement_text = "(Level 4)"
+            else:
+                continue    
+                
+            text_node = NavigableString(replacement_text)
+            img.replace_with(text_node)
+            referenced_tables.add('Table A')
 
-combined = output + tables
 
-# ----------------------- write to json ------------------------
-with open("data/guideline_db.json", "w", encoding="utf-8") as f:
-    json.dump(combined, f, ensure_ascii=False, indent=4)
-    print(f"guideline_db.json file created with {len(combined)} chunks.")
+            
+            
+        # ----------------------get section id----------------------------------
+        parent_sec = p.find_parent(["section",'figure'], id=True)
+        sec_id = parent_sec.get("id") if parent_sec else None
+        
+        
+        #-----------------------get the headings---------------------
+        # special case: # manually finding the "No heading" in the html file to fix the No heading issue
+        # delete the <div><\div> outside this to get the correct heading: <p>Protracted Discontinuation Symptoms and Hyperbolic Tapering Schedules.</p>
+        #it's mostly the "Box"
+        
+        # get the closest heading
+        heading = p.find_previous_sibling(lambda tag: bool(re.match(r'^h[2-6]$', tag.name)))
+        headings = heading.get_text(strip=True) if heading else 'No heading' 
+
+        
+        if 'fig' in sec_id or 'table' in sec_id or 'box' in sec_id:
+            referee_id = headings.lower().replace(".", " ").strip().replace(" ", "_")
+        #while parent still has parents
+        while parent_sec:
+            # print(f"parent_sec: {parent_sec.get('id')}")
+            heading = parent_sec.find_previous_sibling(lambda tag: bool(re.match(r'^h[2-6]$', tag.name)))
+            if heading:
+                headings = heading.get_text(strip=True) + ' > ' + headings
+            parent_sec = parent_sec.find_parent("section", id=True)
+
+        headings = headings.strip().replace('\n', ' ')
+        
+
+        #-----------------------get the text---------------------
+        text = p.get_text(separator=' ', strip=True) # get only text
+        
+        
+        #----------------------- mark Type of the text ------------------------
+        type = 'paragraph'
+        
+        if 'table' in sec_id:  # either table image or table in HTML format
+            if p.get('class') and 'img-box' in p.get('class'):
+                type = 'table image '
+            else: 
+                continue
+            ##################################
+            try:
+                text = find_graph_by_id(graphs, referee_id)
+            except Exception as e:
+                print(f"Error finding graph by ID: {e}")
+                continue
+            ##################################
+
+            # I only have 2 here with parent = <figure>, most of img's parent are <section id = 'table...'>
+        elif p.get('class') and 'img-box' in p.get('class'):
+            type = 'figure image'
+            ##################################
+            try:
+                text = find_graph_by_id(graphs, referee_id)
+            except Exception as e:
+                print(f"Error finding graph by ID: {e}")
+                continue
+            ##################################
+        elif 'box' in sec_id: 
+            type = 'box'
+        
+        if 'fig' in sec_id and type == 'paragraph':
+            continue  # skip paragraphs in figures cause they are already handled in the figure processing
+        
+        #----------------------- get referenced tables ------------------------
+        if not referee_id:
+            all_links = p.find_all('a')
+            for link in all_links:
+                href = link.get('href')
+                if href.startswith('#'):
+                    referenced_tables.add(link.get_text(strip=True))
+        
+                
+        #----------------------- formate the chunks ------------------------
+        chunk = {
+            "text": text,
+            "metadata": {
+            "section": "https://pmc.ncbi.nlm.nih.gov/articles/PMC11351064/#" + sec_id,
+            "type": type,
+            "headings": headings,
+            "referenced_tables": list(referenced_tables),
+            "referee_id": referee_id,
+            }
+        }
+        
+        output.append(chunk)
+    return output
+
+    
+def main():
+    with open("data/processed/graphs.json", "r", encoding="utf-8") as f:
+        graphs = json.load(f)
+    with open("data/processed/tables.json", "r", encoding="utf-8") as f2:
+        tables = json.load(f2)
+        
+    with open('data/processed/tables.json', 'r', encoding='utf-8') as f2:
+        tables = json.load(f2)
+
+# ----------------------- parse the html file ------------------------
+    filename = "data/raw/source.html"
+    with open(filename, "r", encoding="utf-8") as f:
+        html = f.read()
+    soup = BeautifulSoup(html, "html.parser")
+    output = []
+    
+    output.append(parse_title(soup))
+    output = parse_main_article(soup, graphs, output)
+    print(f"Parsed {len(output)} chunks from the main article.")
+    # output = merge_boxes(output)
+    
+
+    # ----------------------- write to json ------------------------
+    combined = output + tables
+    
+    # combined = prepend_headings_to_text(combined)
+    prepend_headings_to_text(combined)
+    append_definition(combined)
+    
+    
+    with open("data/guideline_db.json", "w", encoding="utf-8") as f:
+        json.dump(combined, f, ensure_ascii=False, indent=4)
+        print(f"guideline_db.json file created with {len(combined)} chunks.")
+    
+        
+if __name__ == "__main__":
+    main()

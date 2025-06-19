7b85672f1b5ee2117d69eaa1ee039d8c95aded39
Jiao Zeng <zzejiao@gmail.com>
2025-06-19 00:18:46 -0700

    update run_batched_queries

--- src/Rag.py
+++ src/Rag.py
@@ -154,7 +154,7 @@ def save_faiss_index(embedder_name, index):
 
 # ---------------------------------
 
-def faiss_search(query, embedder, db, index,referenced_table_db, k=3):
+def faiss_search(query, embedder, db, index,referenced_table_db, k=6):
     """_summary_
 
     Args:
@@ -379,3 +379,60 @@ def depression_assistant(query, model_name="meta-llama/Llama-3.3-70B-Instruct-Tu
         response = call_llm(llm_client, prompt, stream_flag, max_tokens=max_tokens, temperature=temperature, top_p=top_p, model_name=model_name)
 
     return results, response
+
+
+def load_queries_and_answers(query_file, answers_file):
+    """
+    Load queries and answers from the provided files.
+    """
+    with open(query_file, 'r') as f:
+        queries = f.readlines()
+    
+    with open(answers_file, 'r') as f:
+        answers = f.readlines()
+    
+    return queries, answers
+
+def write_batched_results(embedder_name, result_path):
+    
+    time0 = time.perf_counter()
+    launch_depression_assistant(embedder_name)
+    print(f"[Time] Launching Depression Assistant took {time.perf_counter() - time0:.2f} seconds.")
+    
+    queries, answers = load_queries_and_answers("data/raw/queries.txt", "data/raw/answers.txt")
+
+    # write results into 2 file, 
+    # Response by {embedder_name} Embedder and LLama3.3 70B
+    # Retrieved Results by {embedder_name} Embedder
+    
+    embedder_filename = embedder_name.replace('/', '_')
+    
+
+    with open(f"{result_path}Retrieved_Results_by_{embedder_filename}.md", "w") as f1, \
+        open(f"{result_path}Response_by_{embedder_filename}.md", "w") as f2:
+
+        for i, query in enumerate(queries):
+            result, response = depression_assistant(query)
+
+            # Write retrieved results
+            f1.write(f"## Query {i+1}\n")
+            f1.write(f"{query.strip()}\n\n")
+            f1.write("## Answer\n")
+            f1.write(f"{answers[i].strip()}\n\n")
+            f1.write("## Retrieved Results\n")
+            
+            for res in result:
+                f1.write(f"\n\n#### {res['section']}\n\n")
+                f1.write(f"{res['text']}\n")
+            f1.write("\n\n---\n\n")
+
+            # Write response
+            f2.write(f"## Query {i+1}\n")
+            f2.write(f"{query.strip()}\n\n")
+            f2.write("## Answer\n")
+            f2.write(f"{answers[i].strip()}\n\n")
+            
+            f2.write(f"## Response\n")
+            f2.write(response)
+            f2.write("\n\n---\n\n")
+            break
--- src/run_batched_queries/write_result_to_file.py
+++ src/run_batched_queries/write_result_to_file.py
@@ -1,28 +1,13 @@
+import pandas as pd
 from Rag import launch_depression_assistant, depression_assistant
-
-def load_queries_and_answers(query_file, answers_file):
-    """
-    Load queries and answers from the provided files.
-    """
-    with open(query_file, 'r') as f:
-        queries = f.readlines()
-    
-    with open(answers_file, 'r') as f:
-        answers = f.readlines()
-    
-    return queries, answers
 
 def write_batched_results(embedder_name, result_path):
     
-    time0 = time.perf_counter()
     launch_depression_assistant(embedder_name)
-    print(f"[Time] Launching Depression Assistant took {time.perf_counter() - time0:.2f} seconds.")
     
-    queries, answers = load_queries_and_answers("data/raw/queries.txt", "data/raw/answers.txt")
-
-    # write results into 2 file, 
-    # Response by {embedder_name} Embedder and LLama3.3 70B
-    # Retrieved Results by {embedder_name} Embedder
+    qa_pairs = pd.read_csv("evaluation/QA_pairs_for_eval/provided_QA_pairs.csv")
+    queries = qa_pairs['query'].tolist()
+    answers = qa_pairs['answer'].tolist()
     
     embedder_filename = embedder_name.replace('/', '_')
     
@@ -33,17 +18,17 @@ def write_batched_results(embedder_name, result_path):
         for i, query in enumerate(queries):
             result, response = depression_assistant(query)
 
-            # Write retrieved results
-            f1.write(f"## Query {i+1}\n")
-            f1.write(f"{query.strip()}\n\n")
-            f1.write("## Answer\n")
-            f1.write(f"{answers[i].strip()}\n\n")
-            f1.write("## Retrieved Results\n")
+            # # Write retrieved results
+            # f1.write(f"## Query {i+1}\n")
+            # f1.write(f"{query.strip()}\n\n")
+            # f1.write("## Answer\n")
+            # f1.write(f"{answers[i].strip()}\n\n")
+            # f1.write("## Retrieved Results\n")
             
-            for res in result:
-                f1.write(f"\n\n#### {res['section']}\n\n")
-                f1.write(f"{res['text']}\n")
-            f1.write("\n\n---\n\n")
+            # for res in result:
+            #     f1.write(f"\n\n#### {res['section']}\n\n")
+            #     f1.write(f"{res['text']}\n")
+            # f1.write("\n\n---\n\n")
 
             # Write response
             f2.write(f"## Query {i+1}\n")
@@ -58,7 +43,17 @@ def write_batched_results(embedder_name, result_path):
 
 
 if __name__ == "__main__":
-    embedder_name = "allenai/longformer-base-4096"
+    #take from command line argument
+    if len(sys.argv) > 1:
+        embedder_name = sys.argv[1]
+        if len(sys.argv) > 2:
+            result_path = sys.argv[2]
+        else:
+            result_path = "evaluation/Results/"
+    else:
+        embedder_name = "Qwen/Qwen3-Embedding-0.6B"
+        result_path = "evaluation/Results/"
+    # embedder_name = "allenai/longformer-base-4096"
     # embedder_name = "emilyalsentzer/Bio_ClinicalBERT"
     # embedder_name = "Qwen/Qwen3-Embedding-0.6B"
     # embedder_name = "all-MiniLM-L6-v2"
@@ -72,7 +67,5 @@ if __name__ == "__main__":
     # embedder_name = 'pritamdeka/S-PubMedBert-MS-MARCO',
     # embedder_name = 'microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext',
     # embedder_name = 'all-MiniLM-L6-v2'
-    
-    result_path = "data/results/week_5_generation/"
     
     write_batched_results(embedder_name, result_path)
